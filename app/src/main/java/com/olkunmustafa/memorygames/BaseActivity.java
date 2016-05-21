27654dc6eb4cbead286dd48e0c63ada15c2e2024
Olkun Mustafa <olkun.mustafa@gmail.com>
2016-05-21 21:33:45 +0300

    First Commit

---
+++ app/src/main/java/com/olkunmustafa/memorygames/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.olkunmustafa.memorygames;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by olkunmustafa on 21/05/16
+ */
+public class BaseActivity extends AppCompatActivity {
+
+
+
+}
---
+++ app/src/main/java/com/olkunmustafa/memorygames/MainActivity.java
@@ -0,0 +1,18 @@
+package com.olkunmustafa.memorygames;
+
+import android.os.Bundle;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+
+    }
+
+}
